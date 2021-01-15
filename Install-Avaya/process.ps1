6f0e842cca7f108518fb2b2135a98e6859e6493f
franklin-gedler <tec.franklin.gedler@gmail.com>
2021-01-15 16:03:27 -0300

    Update 15-01-2021

--- Install-Avaya/process.ps1
+++ Install-Avaya/process.ps1
@@ -6,14 +6,14 @@ Set-Location $currentdirectory
 # Descargar desde Github 
 $token = "569b159288f7c200c33d6472bd5f26a9f2aa7d21"
 $headers = @{Authorization = "token $($token)"}
-Invoke-WebRequest -Headers $headers -Uri "https://raw.githubusercontent.com/franklin-gedler/Scripts-Win10/main/Install-Avaya/ProcessUpdatelatestV2.ps1" -UseBasicParsing -OutFile "$currentdirectory\ProcessUpdatelatestV2.ps1"
+Invoke-WebRequest -Headers $headers -Uri "https://raw.githubusercontent.com/franklin-gedler/Scripts-Win10/main/Install-Avaya/ProcessUpdateLatest.ps1" -UseBasicParsing -OutFile "$currentdirectory\ProcessUpdateLatest.ps1"
 
 #& .\ProcessUpdate.ps1
 
 #Start-Process -Wait PowerShell.exe -ArgumentList "& .\ProcessUpdatelatestV2.ps1"
 
-Start-Process -Wait PowerShell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File "ProcessUpdatelatestV2.ps1"'
+Start-Process -Wait PowerShell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File "ProcessUpdateLatest.ps1"'
 
-Remove-Item ProcessUpdatelatestV2.ps1
+Remove-Item ProcessUpdateLatest.ps1
 
 #Pause
