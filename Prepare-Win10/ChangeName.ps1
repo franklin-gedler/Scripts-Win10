6d4a66343dd05f7da4991b0a5afc618afc908a4c
franklin-gedler <tec.franklin.gedler@gmail.com>
2021-06-16 10:44:59 -0300

    Update 16-06-2021

--- Prepare-Win10/ChangeName.ps1
+++ Prepare-Win10/ChangeName.ps1
@@ -2,6 +2,12 @@ function ChangeName {
     param (
         [String]$1
     )
+
+    Write-Output ""
+    Write-Output " =============================== "
+    Write-Host "   Cambiando Nombre del Equipo   " -ForegroundColor Yellow -BackgroundColor Black
+    Write-Output " =============================== "
+
     $Global:SCompu = (Get-WmiObject win32_bios).SerialNumber
     $Global:NCompu = "$1$SCompu"
     #Write-Output "Nuevo nombre a Setear: $NCompu"
@@ -12,4 +18,11 @@ function ChangeName {
     }
     Rename-Computer -NewName $NCompu -WarningAction SilentlyContinue
     
+    Write-Output ""
+    Write-Output " ######### "
+    Write-Host "   Listo   " -ForegroundColor Green -BackgroundColor Black
+    Write-Output " ######### "
+    Write-Output ""
+    Write-Output "_________________________________________________________________________________________"
+    Write-Output ""
 }
