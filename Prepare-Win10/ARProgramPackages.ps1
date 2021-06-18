dae1d3f5e87a65082d6ebf37305886053fc49c0e
franklin-gedler <tec.franklin.gedler@gmail.com>
2021-06-18 19:45:42 -0300

    Update 18-06-2021

--- Prepare-Win10/ARProgramPackages.ps1
+++ Prepare-Win10/ARProgramPackages.ps1
@@ -9,12 +9,12 @@ function ARProgramPackages {
 
         Write-Output ""
         Write-Output " ========================== "
-        Write-Output "    Instalando Office365    " -ForegroundColor Yellow -BackgroundColor Black
+        Write-Host "    Instalando Office365    " -ForegroundColor Yellow -BackgroundColor Black
         Write-Output " ========================== "
         Start-Process -Wait C:\Users\admindesp\Downloads\Office365\setup.exe `
             -ArgumentList '/configure C:\PrepareWin10\Office365\installOfficeBusRet64.xml'
     }
-
+    Pause
     Write-Output ""
     Write-Output " ================================= "
     Write-Host "    Instalando FusionInventory     " -ForegroundColor Yellow -BackgroundColor Black
