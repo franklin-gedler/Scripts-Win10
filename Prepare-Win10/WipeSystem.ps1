b62f249c42df2e4dee4879aa88cc4951ae2d04c0
franklin-gedler <tec.franklin.gedler@gmail.com>
2021-06-22 20:08:36 -0300

    Update 22-06-2021

--- Prepare-Win10/WipeSystem.ps1
+++ Prepare-Win10/WipeSystem.ps1
@@ -4,11 +4,11 @@ function WipeSystem {
     Start-Sleep -Seconds 5
     Remove-Item -LiteralPath C:\Windows\Setup\scripts -Recurse -Force
     Remove-Item -LiteralPath C:\PrepareWin10\ -Recurse -Force
-'@ | Add-Content C:\PrepareWin10\AutoDelete.ps1
+'@ | Add-Content C:\PS\AutoDelete.ps1
     
     $action = New-ScheduledTaskAction -Execute 'Powershell.exe' `
-            -WorkingDirectory "C:\Program Files\Dell\" `
-            -Argument '-NoProfile -ExecutionPolicy Bypass -File C:\PrepareWin10\AutoDelete.ps1'
+            -WorkingDirectory "C:\PS\" `
+            -Argument '-NoProfile -ExecutionPolicy Bypass -File C:\PS\AutoDelete.ps1'
 
     $trigger =  New-ScheduledTaskTrigger -AtStartup
 
