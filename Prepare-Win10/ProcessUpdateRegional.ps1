4e9d9faa4638bb1a984fb79a5d856bb1d695856c
franklin-gedler <tec.franklin.gedler@gmail.com>
2021-07-05 14:06:18 -0300

    Update 5-07-2021

--- Prepare-Win10/ProcessUpdateRegional.ps1
+++ Prepare-Win10/ProcessUpdateRegional.ps1
@@ -22,9 +22,9 @@ $code=@'
   public static extern void SetThreadExecutionState(uint esFlags);
 '@
 
-$ste = Add-Type -memberDefinition $code -name System -namespace Win32 -passThru
-$ES_CONTINUOUS = [uint32]"0x80000000" 
-#$ES_AWAYMODE_REQUIRED = [uint32]"0x00000040" 
+$ste = Add-Type -memberDefinition $code -name System -namespace Win32 -ErrorAction SilentlyContinue
+$ES_CONTINUOUS = [uint32]"0x80000000"
+#$ES_AWAYMODE_REQUIRED = [uint32]"0x00000040"
 $ES_DISPLAY_REQUIRED = [uint32]"0x00000002"
 $ES_SYSTEM_REQUIRED = [uint32]"0x00000001"
 
@@ -33,6 +33,7 @@ $ste::SetThreadExecutionState($ES_CONTINUOUS -bor $ES_SYSTEM_REQUIRED -bor $ES_D
 #$ste::SetThreadExecutionState($ES_CONTINUOUS)
 
 #-------------------------------------------------------------------------------------------------------------------
+
 
 function DownloadModules {
     param (
