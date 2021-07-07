d609d9a3d64ebd24377de644aca65bf0ce70cb9e
franklin-gedler <tec.franklin.gedler@gmail.com>
2021-07-07 13:54:33 -0300

    Update 7-07-2021

--- Prepare-Win10/ChangeName.ps1
+++ Prepare-Win10/ChangeName.ps1
@@ -9,38 +9,55 @@ function ChangeName {
     Write-Output " =============================== "
 
     #$VeryPCI = Get-Content C:\Users\admindesp\Desktop\PCI.txt
-                
-    switch ($PCI) {
+    
+    if (!$PCI){
 
-        1{
-            # Es PCI
+        $SCompu = (Get-WmiObject win32_bios).SerialNumber
+        $Global:NCompu = "$1$SCompu"
+        
+        while (!$SCompu) {
             $SCompu = (Get-WmiObject win32_bios).SerialNumber
-            $Global:NCompu = $1 + 'PCI' + $SCompu
-            
-            while (!$SCompu) {
+            $Global:NCompu = "$1$SCompu"
+        }
+
+        Rename-Computer -NewName $NCompu -WarningAction SilentlyContinue
+
+    }else {
+        
+        switch ($PCI) {
+
+            1{
+                # Es PCI
                 $SCompu = (Get-WmiObject win32_bios).SerialNumber
                 $Global:NCompu = $1 + 'PCI' + $SCompu
                 
+                while (!$SCompu) {
+                    $SCompu = (Get-WmiObject win32_bios).SerialNumber
+                    $Global:NCompu = $1 + 'PCI' + $SCompu
+                    
+                }
+    
+                Rename-Computer -NewName $NCompu -WarningAction SilentlyContinue
             }
-
-            Rename-Computer -NewName $NCompu -WarningAction SilentlyContinue
-        }
-        
-        2{
-            # No es PCI
-            $SCompu = (Get-WmiObject win32_bios).SerialNumber
-            $Global:NCompu = "$1$SCompu"
             
-            while (!$SCompu) {
+            2{
+                # No es PCI
                 $SCompu = (Get-WmiObject win32_bios).SerialNumber
                 $Global:NCompu = "$1$SCompu"
                 
+                while (!$SCompu) {
+                    $SCompu = (Get-WmiObject win32_bios).SerialNumber
+                    $Global:NCompu = "$1$SCompu"
+                    
+                }
+    
+                Rename-Computer -NewName $NCompu -WarningAction SilentlyContinue
+                #Write-Output $NCompu > C:\PrepareWin10\NCompu.txt
             }
-
-            Rename-Computer -NewName $NCompu -WarningAction SilentlyContinue
-            #Write-Output $NCompu > C:\PrepareWin10\NCompu.txt
         }
     }
+
+    
 
     Write-Output ""
     Write-Output " ######### "
