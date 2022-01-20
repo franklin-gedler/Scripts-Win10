e97f1fa468336faac2dbbf3a76db30ef3be7b9f0
franklin-gedler <60144812+franklin-gedler@users.noreply.github.com>
2022-01-20 11:51:51 -0300

    Update process.ps1

--- Install-Avaya/process.ps1
+++ Install-Avaya/process.ps1
@@ -4,36 +4,10 @@ Set-Location $currentdirectory
 #######################################################################################################################
 
 # Descargar desde Github 
-$token = "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"
+$token = "Token Generado desde Gitgub"
 $headers = @{Authorization = "token $($token)"}
 Invoke-WebRequest -Headers $headers -Uri "https://raw.githubusercontent.com/franklin-gedler/Scripts-Win10/main/Install-Avaya/ProcessUpdateLatest.ps1" -UseBasicParsing -OutFile "$currentdirectory\ProcessUpdateLatest.ps1"
-
-#& .\ProcessUpdate.ps1
-
-#Start-Process -Wait PowerShell.exe -ArgumentList "& .\ProcessUpdatelatestV2.ps1"
 
 Start-Process -Wait PowerShell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File "ProcessUpdateLatest.ps1"'
 
 Remove-Item ProcessUpdateLatest.ps1
-
-#Pause
-
-
-<#
-# Destruction Block
-mkdir $env:TEMP\paraborrar\ -Force
-Write-Output $currentdirectory > $env:TEMP\paraborrar\pathfiles.txt
-
-Write-Output {
-    
-    $pathfiles = Get-Content $env:TEMP\paraborrar\pathfiles.txt
-    Remove-Item $pathfiles\ProcessUpdateLatest.ps1 -Force
-
-} > $env:TEMP\paraborrar\borrado.ps1
-
- 
-
-& $env:TEMP\paraborrar\borrado.ps1
-
-
-#>
